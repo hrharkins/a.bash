62c33b034a641d9fff54afdd3ab1724dbe0a17f1
Rich Harkins <rich.harkins@gmail.com>
2022-08-07 13:16:22 +0000

    Globalized ABASHED

--- abashed/v0.1/a.bash
+++ abashed/v0.1/a.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 declare _ACORN="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/a.corn.bash" &&
-declare -A ABASHED && [ "${ABASHED[$_ACORN]:+_}" ] || . "$_ACORN" && 
+declare -Ag ABASHED && [ "${ABASHED[$_ACORN]:+_}" ] || . "$_ACORN" && 
 if [ "${BASH_SOURCE[1]}" ]
 then "ABASHED_v0_1:as-source" "$@"
 else "ABASHED_v0_1:main" "$@"; fi
